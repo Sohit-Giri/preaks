878694ff952aa56fa0fab527f4662096c421cf3c
Sohit Giri <129953866+Sohit-Giri@users.noreply.github.com>
2024-12-12 06:26:00 +0545

    changes for static optimization

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from django.conf.urls.static import static
+from sohit import settings
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
